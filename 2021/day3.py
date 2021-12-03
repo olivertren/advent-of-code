b66c55430470417c2b75e79b2e245e965c8d3871
Oliver Ren <olivertren@gmail.com>
2021-12-03 00:55:18 -0500

    Day 3 Update: I did with less code so I have techniques in the bag for the future.

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,72 +1,41 @@
 import datetime
+from collections import Counter
 
 import numpy as np
 
 from utils import get_input, print_answers
 
 
+def get_most_common_at_index(i, ns):
+    counter = Counter(list(map(lambda x: x[i], ns)))
+    return int(counter["1"] >= counter["0"])
+
+
 def part_one(ns):
     counter = np.zeros(len(ns[0]))
-    for n in ns:
-        for i, b in enumerate(n):
-            if b == "1":
-                counter[i] += 1
-            else:
-                counter[i] -= 1
-    gamma = ""
-    epsilon = ""
-    for i in counter:
-        if i > 0:
-            gamma += "1"
-            epsilon += "0"
-        else:
-            gamma += "0"
-            epsilon += "1"
+    for i in range(len(counter)):
+        counter[i] = get_most_common_at_index(i, ns)
 
-    return int(gamma, 2) * int(epsilon, 2)
+    gamma = int("".join((counter == 1).astype(int).astype(str)), 2)
+    epsilon = int("1" * len(counter), 2) ^ gamma
+
+    return gamma * epsilon
 
 
 def part_two(ns):
+    o = ns
+    c = ns
     i = 0
-    ns2 = ns
-    while len(ns) > 1:
-        c = 0
-        for n in ns:
-            if n[i] == "1":
-                c += 1
-            else:
-                c -= 1
-        ns_ = []
-        for n in ns:
-            if c >= 0 and n[i] == "1":
-                ns_.append(n)
-            elif c < 0 and n[i] == "0":
-                ns_.append(n)
-        ns = ns_
+    while len(o) > 1 or len(c) > 1:
+        mco = get_most_common_at_index(i, o)
+        mcc = get_most_common_at_index(i, c)
+        lcc = 1 ^ mcc
+
+        o = list(filter(lambda x: str(mco) == x[i], o)) if len(o) > 1 else o
+        c = list(filter(lambda x: str(lcc) == x[i], c)) if len(c) > 1 else c
         i += 1
 
-    o = ns[0]
-
-    ns = ns2
-    i = 0
-    while len(ns) > 1:
-        c = 0
-        for n in ns:
-            if n[i] == "1":
-                c += 1
-            else:
-                c -= 1
-        ns_ = []
-        for n in ns:
-            if c >= 0 and n[i] == "0":
-                ns_.append(n)
-            elif c < 0 and n[i] == "1":
-                ns_.append(n)
-        ns = ns_
-        i += 1
-
-    c = ns[0]
-    return int(o, 2) * int(c, 2)
+    return int(o[0], 2) * int(c[0], 2)
 
 
 def print_answers_for_input(input_):
